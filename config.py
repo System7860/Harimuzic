6710b180ed5e9dfd150ecc88a254e5055e16c8d2
System7860 <system677155@gmail.com>
2025-05-26 12:31:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @BRANDRD_ROBOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "7903596664"))
+OWNER_ID = int(getenv("OWNER_ID", "7638430820"))
 
 OWNER_USERNAME = getenv("OWNER_USERNAME","xeno_kakarot")
 
